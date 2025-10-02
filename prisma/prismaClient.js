a4294aee81448f8cd02d37b0a4391c907ffa8dad
angeld1603 <angeldavidjulioorozco58@gmail.com>
2025-10-02 18:39:03 -0500

    Agregar archivo prismaClient.js para crear una instancia de Prisma Client y facilitar las operaciones de base de datos

---
+++ prisma/prismaClient.js
@@ -0,0 +1,5 @@
+import { PrismaClient } from '@prisma/client';
+
+// Create a new Prisma Client instance to interact with the database
+// This instance can be used to perform database operations throughout the application
+export const prisma = new PrismaClient();
